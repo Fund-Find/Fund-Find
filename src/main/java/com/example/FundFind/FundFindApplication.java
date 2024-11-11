849a96c1312a930fbcebf078bd1c893c5e754609
taeho5825 <xogh3322@naver.comm>
2024-11-11 12:31:44 +0900

    first commit

---
+++ src/main/java/com/example/FundFind/FundFindApplication.java
@@ -0,0 +1,13 @@
+package com.example.FundFind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundFindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FundFind/FundFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FundFind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
