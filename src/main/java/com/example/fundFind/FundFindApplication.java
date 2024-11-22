5768c2c4bc6c766bdf32fab57c8af7017bea8ba3
taeho5825 <xogh3322@naver.comm>
2024-11-22 09:30:41 +0900

    ETF_Test

--- src/main/java/com/example/fundFind/FundFindApplication.java
+++ src/main/java/com/example/fundFind/FundFindApplication.java
@@ -2,8 +2,12 @@ package com.example.fundFind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.domain")  // 변경
+@EnableJpaRepositories(basePackages = "com.example")
+@EntityScan(basePackages = "com.example")
 public class FundFindApplication {
 
 	public static void main(String[] args) {
